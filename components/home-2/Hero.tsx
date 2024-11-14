fba3f82b3d5a254cb032ad9bd0318f77e3720be6
Kaushik <kaushik@techmindscape.com>
2024-11-14 09:05:56 +0530

    updated code

--- components/home-2/Hero.tsx
+++ components/home-2/Hero.tsx
@@ -91,7 +91,7 @@ const Hero = () => {
             >
               <CalendarDaysIcon className="w-6 h-6 text-gray-600" />
             </button>
-          </div>
+          </div>z
 
 <<<<<<< HEAD
           {/* <div className="w-full md:w-[55%] xl:w-[27%]">
